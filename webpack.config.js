b9037309bd4fe65f2c2808d391a180a63e58468b
Krishank Dwivedi 2 <kdwivedi2@sapient.com>
2017-01-11 20:49:20 +0530

    Relative paths are not trustable so adding configration in web pack to set alias paths similar as we set base and alias path in require.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
+var path = require('path');
 module.exports = {
     entry: './www/js/main',
     output: {
         path: 'www/dist',
         filename: 'main.bundle.js'
-    }
+    },
+    resolve: {
+        root: path.resolve(__dirname),
+        alias: {
+            lib: 'www/lib',
+            angular: 'www/lib/angular/angular',
+            angularMocks: 'www/lib/angular-mocks',
+            modules: 'www/modules',
+            enrollmentModule: 'www/modules/enrollment'
+        },
+        extensions: ['', '.js', '.jsx']
+    },
+    watch: true
 };
--- www/modules/enrollment/js/controller/home-controllers.js
+++ www/modules/enrollment/js/controller/home-controllers.js
@@ -1,6 +1,21 @@
 define([], function() {  
     'use strict';  
     return function($scope, $stateParams, $http) {   
+        $scope.name = 'Krishank';
         console.log('Home Ctrl');
     };
 });
+
+// define([], function() {  
+//     'use strict';  
+
+//     var homeControllers = {
+//         defaults: 1,
+//         someMethod: function($scope, $stateParams, $http) {
+//             $scope.name = 'Krishank';
+//             console.log('Home Ctrl');
+//         }
+//     };
+//     homeControllers.someMethod();
+//     return homeControllers;
+// });
